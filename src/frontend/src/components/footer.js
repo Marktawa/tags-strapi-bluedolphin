d24da5aab9128589507f09f6fc220bb9c6e3fc0d
McTawa <markmunyaka@gmail.com>
2022-10-07 13:29:38 +0000

    Added header and footer

---
+++ src/frontend/src/components/footer.js
@@ -0,0 +1,32 @@
+// ./src/components/footer.js
+
+import React from "react";
+import styled from "styled-components";
+
+const Footer = styled.footer`
+  height: 55px;
+  background-color: #1a124b;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h5 {
+      padding-top : 10px;
+      font-size: 1.2rem;
+  }
+`;
+
+const FooterComponent = (props) => (
+  <Footer>
+    <h5>
+      {" "}
+      Designed and built by{" "}
+      <a href="https://github.com/Marktawa"> Mark Munyaka </a>{" "}
+    </h5>
+  </Footer>
+);
+
+export default FooterComponent;
---
+++ src/frontend/src/components/header.js
@@ -0,0 +1,43 @@
+// ./src/components/header.js
+
+import React from "react";
+import styled from "styled-components";
+import { Link } from "gatsby";
+
+const Header = styled.header`
+  height: 55px;
+  background-color: #1a124b;
+  width: 100%;
+  color: white;
+  display: flex;
+  align-items: center;
+  padding: 0 2rem;
+  box-shadow: 0 2px 3px white;
+  nav {
+    padding-top: 10px;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    h5 {
+      font-size: 1.1rem;
+    }
+  }
+`;
+
+const HeaderComponent = (props) => (
+  <Header>
+    <nav>
+      <div>
+        <Link to="/">
+          <h5> STRAPI ECOM APP </h5>
+        </Link>
+      </div>
+
+      <div>
+        <p> About </p>
+      </div>
+    </nav>
+  </Header>
+);
+
+export default HeaderComponent;
