51817d536695e0d6887cf49224a04255d96cf2bf
McTawa <markmunyaka@gmail.com>
2022-10-08 08:34:41 +0000

    Minor tweaks

--- src/frontend/src/styles.js
+++ src/frontend/src/styles.js
@@ -1,17 +1,18 @@
 // ./src/styles.js
 
 import styled from "styled-components";
-    
+
 export const Cards = styled.ul`
     display: grid;
     padding-top: 1rem;
+    padding-bottom: 5rem;
     grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
     grid-gap: 2rem 1rem;
     place-items: center;
 `;
-    
+
 export const Item = styled.div`
-    height: 370px;
+    height: 400px;
     width: 300px;
     border-radius: 7px;
     box-shadow: 0 2px 3px #c0c0c0;
@@ -26,7 +27,7 @@ export const Item = styled.div`
     }
     }
 `;
-    
+
 export const Button = styled.button`
     background: #1d1b84;
     border: 1px solid #1d1b84;
@@ -34,13 +35,13 @@ export const Button = styled.button`
     border-radius: 3px;
     padding: 0.3rem 1rem;
 `;
-    
+
 export const Image = styled.img`
     height: 250px;
     width: 250px;
     object-fit: contain;
 `;
-    
+
 export const Flex = styled.div`
     display: flex;
     width: 100%;
@@ -48,15 +49,15 @@ export const Flex = styled.div`
     flex-direction: ${(props) => props.direction};
     justify-content: ${(props) => props.justify};
 `;
-    
+
 export const Banner = styled.div`
-    padding: 2rem 3rem;
+    padding: 0.4rem 0.5rem;
     text-align: center;
     h4 {
     font-weight: 600;
     }
 `;
-    
+
 export const CategoryCtn = styled.div`
     width: 20rem;
     padding: .5rem 1rem;
@@ -65,8 +66,7 @@ export const CategoryCtn = styled.div`
     border-radius: 5px;
     box-shadow: 0 2px 3px #c0c0c0;
 `
-    
+
 export const Container = styled.div`
     background: #ebf4fd;
-    height: calc(100vh - 110px);
 `;
